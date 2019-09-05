5c978271400db8850f953817474d7e4be318d41b
Austin Blaine <theaustinblaine@gmail.com>
2019-09-05 10:00:39 -0400

    deleted unused files and attempting to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,8 @@ const IndexPage = () => (
           <div className="col-lg-8 mx-auto">
             <h2 className="text-white mb-4">Hello, Meet Austin</h2>
             <p className="text-white-50">
-              I develop web applications
+              I develop web applications with a programmatic mind. With over 15 years experience in audio and the music industry, I have developed a meticulous
+              work flow in everything I do. Since making the jump to becoming a developer, I've found many parallels between the two industries.
             </p>
           </div>
         </div>
@@ -59,9 +60,7 @@ const IndexPage = () => (
             <div className="featured-text text-center text-lg-left">
               <h4>indiEPK</h4>
               <p className="text-black-50 mb-0">
-                Grayscale is open source and MIT licensed. This means you can
-                use it for any project - even commercial projects! Download it,
-                customize it, and publish your website!
+                indiEPK is a web based press kit platform for independent musicians to showcase relevant material and events without having the need to play the social media game.
               </p>
             </div>
           </div>
@@ -77,9 +76,7 @@ const IndexPage = () => (
                 <div className="project-text w-100 my-auto text-center text-lg-right">
                   <h4 className="text-white">Delay</h4>
                   <p className="mb-0 text-white-50">
-                    Another example of a project with its respective
-                    description. These sections work well responsively as well,
-                    try this theme on a small screen!
+                    Delay is an online guitar store, similar to the store Reverb. I built this application to attempt creating a webstore with functional cart and cart total.
                   </p>
                   <hr className="d-none d-lg-block mb-0 mr-0" />
                 </div>
@@ -98,8 +95,7 @@ const IndexPage = () => (
                 <div className="project-text w-100 my-auto text-center text-lg-left">
                   <h4 className="text-white">Gascap</h4>
                   <p className="mb-0 text-white-50">
-                    An example of where you can put an image of a project, or
-                    anything else, along with a description.
+                    GAscap is a mock Performance Rights Organization, similar to BMI or... you guessed it... ASCAP. This was my first attempt at a full-stack webapp. With my history in the music industry, I saw the PRO model as a simple thing to recreate with my new skills.
                   </p>
                   <hr className="d-none d-lg-block mb-0 ml-0" />
                 </div>
@@ -118,9 +114,7 @@ const IndexPage = () => (
                 <div className="project-text w-100 my-auto text-center text-lg-right">
                   <h4 className="text-white">Combine 3 & 1</h4>
                   <p className="mb-0 text-white-50">
-                    Another example of a project with its respective
-                    description. These sections work well responsively as well,
-                    try this theme on a small screen!
+                    This is a simple take on Connect 4. I didn't want to get sued by the Hasbro's though, so I changed the name.
                   </p>
                   <hr className="d-none d-lg-block mb-0 mr-0" />
                 </div>
